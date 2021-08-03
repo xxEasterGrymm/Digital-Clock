db1eea382f576b4e0e410aa8ef06ea72f8f0c870
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-08-03 10:13:37 +0530

    initial commit

---
+++ src/MyWindow.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+public class MyWindow extends JFrame {
+    private JLabel label;
+    private Font font = new Font("Times New Roman", Font.BOLD, 30);
+
+    MyWindow() {
+        setTitle("Digital Clock");
+        setSize(570, 300);
+        setLocation(500, 200);
+        createGUI();
+        startClock();
+        setVisible(true);
+    }
+
+    public void createGUI() {
+        label = new JLabel("Digital Clock");
+        label.setFont(font);
+        setLayout(new GridLayout(2, 1));
+        add(label);
+    }
+
+    public void startClock() {
+
+        Timer timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String dateTime = new Date().toLocaleString();
+                label.setText(dateTime);
+            }
+        });
+        timer.start();
+        JLabel ist = new JLabel("IST - Indian Standard Time (UTC + 5:30)");
+        ist.setFont(font);
+        add(ist);
+    }
+}
